34d02fcce5e752716a32216cc6e7ad4a2cd98edb
Semen Saifutdinov <saifutdinov2006@mail.ru>
2024-11-26 14:07:41 +0300

    Create model User

--- authapp/models.py
+++ authapp/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    Класс модели пользователя
+    """
+    username = models.CharField("username", max_length=150,
+                                null=True, blank=True)
+    middle_name = models.CharField("middle name",
+                                   max_length=150,
+                                   blank=True)
+    email = models.EmailField("email address",
+                              unique=True)  # переопределение
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ['username']
+
+
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
