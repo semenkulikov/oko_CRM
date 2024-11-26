5cb1945fe46fd7259ad5bfe87ec81219eee7bc06
Semen Saifutdinov <saifutdinov2006@mail.ru>
2024-11-26 17:58:35 +0300

    Authapp urls and view added

--- authapp/forms.py
+++ authapp/forms.py
@@ -8,6 +8,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.template import loader
 
 
+
 class UserLoginForm(AuthenticationForm):
     """
     Форма для авторизации пользователя
@@ -23,36 +24,49 @@ class UserLoginForm(AuthenticationForm):
         fields = ('username', 'password')
 
 
-    def clean(self):
-        """
-        Переопределение метода для отображения ошибки при неактивном
-        пользователе
-        """
-        username = self.cleaned_data.get("username")
-        password = self.cleaned_data.get("password")
-        if username is not None and password:
-            self.user_cache = authenticate(
-                self.request, username=username, password=password
-            )
-            if self.user_cache is None:
-                try:  # попытка найти пользователя с таким email
-                    user_not_active = User.objects.get(email=username)
-                except ObjectDoesNotExist:
-                    user_not_active = None
-                if user_not_active is not None and \
-                        user_not_active.check_password(password):
-                    # если пользователь с таким username(email) существует,
-                    # и введён верный пароль, но его учетная запись неактивна
-                    self.confirm_login_allowed(user_not_active)
-                else:
-                    raise self.get_invalid_login_error()
-        return self.cleaned_data
+    # def clean(self):
+    #     """
+    #     Переопределение метода для отображения ошибки при неактивном
+    #     пользователе
+    #     """
+    #     username = self.cleaned_data.get("username")
+    #     password = self.cleaned_data.get("password")
+    #     if username is not None and password:
+    #         self.user_cache = authenticate(
+    #             self.request, username=username, password=password
+    #         )
+    #         if self.user_cache is None:
+    #             try:  # попытка найти пользователя с таким email
+    #                 user_not_active = User.objects.get(email=username)
+    #             except ObjectDoesNotExist:
+    #                 user_not_active = None
+    #             if user_not_active is not None and \
+    #                     user_not_active.check_password(password):
+    #                 # если пользователь с таким username(email) существует,
+    #                 # и введён верный пароль, но его учетная запись неактивна
+    #                 self.confirm_login_allowed(user_not_active)
+    #             else:
+    #                 raise self.get_invalid_login_error()
+    #     return self.cleaned_data
 
 
 class UserSignUpForm(UserCreationForm):
     """
     Форма для регистрации пользователя
     """
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['email'].widget.attrs['placeholder'] = 'E-mail'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Password'
+        self.fields['password2'].widget. \
+            attrs['placeholder'] = 'Password confirmation'
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Surname'
+        self.fields['middle_name'].widget.attrs['placeholder'] = 'Middle name'
+        self.fields['last_name'].required = True
+        self.fields['first_name'].required = True
+        for name_field, field in self.fields.items():
+            field.widget.attrs['class'] = 'user-input'
 
     class Meta:
         model = User
--- authapp/views.py
+++ authapp/views.py
@@ -5,15 +5,17 @@ from django.conf import settings
 from django.contrib.auth import login
 from django.contrib.auth.views import PasswordResetView, \
     PasswordResetConfirmView
-from django.views.generic import CreateView
+from django.views.generic import CreateView, DetailView
 from django.http import HttpResponseRedirect
 from django.urls import reverse_lazy, \
     reverse
-from authapp.forms import UserLoginForm
+from authapp.forms import UserLoginForm, UserSignUpForm
 from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpResponse
 from django.contrib.sites.shortcuts import get_current_site
 import logging
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 
 logger = logging.getLogger('global')
 
@@ -24,7 +26,6 @@ class UserLoginView(LoginView):
 
     template_name = 'authapp/login.html'
     form_class = UserLoginForm
-    redirect_authenticated_user = True
 
 
     def form_valid(self, form: UserLoginForm):
@@ -35,114 +36,68 @@ class UserLoginView(LoginView):
         logger.info(f"Пользователь {self.request.user.username} успешно авторизовался!")
         return HttpResponseRedirect(self.get_success_url())
 
+    def post(self, request, *args, **kwargs) -> HttpResponse:
+        form = self.form_class(data=request.POST)
+        logger.info(f"Попытка авторизации! {form.data.get('username')}: {form.data.get('password')}")
+        if form.is_valid():  # форма прошла валидацию
+            user = form.get_user()
+            login(request, user)
+            if user.is_superuser:
+                logger.info(f"Администратор {user.first_name} {user.last_name} успешно авторизовался!")
+                return HttpResponseRedirect(reverse('admin-panel'))
+            else:
+                logger.info(f"Пользователь {user.first_name} {user.last_name} успешно авторизовался!")
+                return HttpResponseRedirect(reverse('authapp:profile'))
+        else:
+            messages.error(request, *list(form.errors.values()))
+        return render(request, self.template_name, {'form': form})
 
 
-# class UserLogoutView(LogoutView):
-#     """
-#     Выход пользователя из учётной записи
-#     """
-#     next_page = '/'
-#
-#
-# class UserSignUpView(CreateView):
-#     """
-#     Регистрация пользователя
-#     """
-#     model = User
-#     template_name = 'authapp/registr.html'
-#     form_class = UserSignUpForm
-#     success_url = reverse_lazy('authapp:login')
-#     rep_cart = RepCart()
-#
-#     def get(self, request, *args, **kwargs) \
-#             -> HttpResponse:
-#         """
-#         Метод для отображения страницы регистрации и формы
-#         """
-#         form = self.form_class(data=request.GET)
-#         if request.user.is_authenticated:
-#             return HttpResponseRedirect(reverse('coreapp:index'))
-#         return render(request, self.template_name, {'form': form})
-#
-#     def post(self, request, *args, **kwargs) -> HttpResponse:
-#         form = self.form_class(data=request.POST)
-#         if form.is_valid():  # форма прошла валидацию
-#             user = form.save(commit=False)
-#             user.is_active = False  # деактивация пользователя
-#             user.activation_key = generate_random_string()
-#             user.save()
-#             current_site = get_current_site(request)
-#             site_name = current_site.name
-#             protocol = request.scheme
-#             domain = current_site.domain
-#             send_verif_link.delay(protocol, domain, site_name, user.email,
-#                                   user.activation_key,
-#                                   user.first_name, user.last_name)
-#             # ссылка создана и отправлено сообщение
-#             messages.success(request, messages_dict['reg_success'])
-#             return HttpResponseRedirect(reverse('authapp:login'))
-#         else:  # при наличии ошибок в форме
-#             messages.error(request, *list(form.errors.values()))
-#         return render(request, self.template_name, {'form': form})
-#
-#
-# def verify_user(request, **kwargs) -> HttpResponse:
-#     """
-#     Активация учетной записи
-#     :return: Response
-#     :rtype: HttpResponse
-#     """
-#     if request.method == 'GET':
-#         email = kwargs.get('email')  # мейл из запроса
-#         activate_key = kwargs.get('key')  # ключ из запроса
-#         user = User.objects.get(email=email)
-#         if user and user.activation_key == activate_key:
-#             if not user.is_activation_key_expires:
-#                 # если еще не прошло 72 часа
-#                 # с момента регистрации и ключи одинаковые
-#                 user.activation_key = ""
-#                 user.is_active = True  # активация пользователя
-#                 user.activation_key_expires = None
-#                 user.save()
-#                 login(request, user)  # вход в учетную запись
-#                 session_products = request.session.get('products')
-#                 AddToCart.move_from_session(user, session_products)
-#             else:
-#                 messages.error(request, messages_dict['activate_error'])
-#     return HttpResponseRedirect(reverse('coreapp:index'))
-#
-#
-# class UserPassResetView(PasswordResetView):
-#     """
-#     Класс для отработки отправки токена для смены пароля на
-#     электронную почту.
-#     """
-#     form_class = UserResetPasswordForm
-#     template_name = "authapp/forgot_password.html"
-#     from_email = settings.EMAIL_HOST_USER
-#     html_email_template_name = "authapp/email/reset_confirm.html"
-#     email_template_name = 'authapp/email/reset_confirm.html'
-#     success_url = reverse_lazy('authapp:login')
-#
-#     def form_valid(self, form):
-#         """
-#         Метод, вызываемый при валидации формы
-#         """
-#         messages.success(self.request, messages_dict['pass_link'])
-#         return super().form_valid(form)
-#
-#
-# class UserPassChangeView(PasswordResetConfirmView):
-#     """
-#     Класс для смены пароля
-#     """
-#     form_class = UserSetPasswordForm
-#     template_name = "authapp/set_password.html"
-#     success_url = reverse_lazy('authapp:login')
-#
-#     def form_valid(self, form):
-#         """
-#         Метод, вызываемый при валидации формы
-#         """
-#         messages.success(self.request, messages_dict['pass_change_success'])
-#         return super().form_valid(form)
+
+class UserLogoutView(LogoutView):
+    """
+    Выход пользователя из учётной записи
+    """
+    next_page = '/'
+
+
+class UserSignUpView(CreateView):
+    """
+    Регистрация пользователя
+    """
+    model = User
+    template_name = 'authapp/registr.html'
+    form_class = UserSignUpForm
+    success_url = reverse_lazy('authapp:login')
+
+    def get(self, request, *args, **kwargs) \
+            -> HttpResponse:
+        """
+        Метод для отображения страницы регистрации и формы
+        """
+        form = self.form_class(data=request.GET)
+        if request.user.is_authenticated:
+            return HttpResponseRedirect(reverse('index'))
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request, *args, **kwargs) -> HttpResponse:
+        form = self.form_class(data=request.POST)
+        if form.is_valid():  # форма прошла валидацию
+            user = form.save(commit=False)
+            user.save()
+            logger.info(f'Пользователь {user.first_name} {user.last_name} зарегистрирован')
+            return HttpResponseRedirect(reverse('authapp:login'))
+        else:  # при наличии ошибок в форме
+            messages.error(request, *list(form.errors.values()))
+        return render(request, self.template_name, {'form': form})
+
+
+class ProfileDetailView(LoginRequiredMixin, DetailView):
+    """
+    View класс для отображения информации об аккаунте
+    """
+    queryset = User
+    template_name = 'authapp/account.html'
+
+    def get_object(self, queryset=None):
+        return self.request.user
