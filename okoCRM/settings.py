d52d0820ec1f26961bf57bf52d6e95c6b2dcffae
Semen Saifutdinov <saifutdinov2006@mail.ru>
2024-11-26 17:57:17 +0300

    Fix settings

--- okoCRM/settings.py
+++ okoCRM/settings.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
+    'authapp',
 ]
 
 MIDDLEWARE = [
@@ -162,3 +164,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL = 'authapp.User'
--- okoCRM/urls.py
+++ okoCRM/urls.py
@@ -14,9 +14,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-# from django.contrib import admin
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include
+from .views import IndexView
 
 urlpatterns = [
-    #    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin-panel'),
+    path('auth/', include('authapp.urls')),
+    # path("accounts/", include("django.contrib.auth.urls")),
+    path('', IndexView.as_view(), name='index'),
 ]
